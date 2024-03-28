37f6d6a69cf0f0c45cbf7f103ec887378d1b620c
GDayan <daykanot@gmail.com>
2024-03-28 14:50:42 +0300

    done task

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -1,5 +1,8 @@
 package com.epam.mjc.stage0;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * Here are the tasks for working with the arrays.
  * <p>
@@ -7,91 +10,65 @@ package com.epam.mjc.stage0;
  */
 public class ArrayTasks {
 
-    /**
-     * Return a String[] array that will list all the seasons of the year, starting with winter.
-     */
     public String[] seasonsArray() {
-
+        return new String[]{"winter", "spring", "summer", "autumn"};
     }
 
-    /**
-     * Generate an int[] array of consecutive positive integers
-     * starting at 1 of the given length (length parameter > 0).
-     * <p>
-     * Example:
-     * <p>
-     * length = 1  -> [1]
-     * length = 3  -> [1, 2, 3]
-     * length = 5  -> [1, 2, 3, 4, 5]
-     */
     public int[] generateNumbers(int length) {
-
+        int[] result = new int[length];
+        for (int i = 0; i < length; i++) {
+            result[i] = i + 1;
+        }
+        return result;
     }
 
-    /**
-     * Find the sum of all elements of the int[] array.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [1, 3, 5]   -> sum = 9
-     * arr = [5, -3, -4] -> sum = -2
-     */
     public int totalSum(int[] arr) {
-
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
     }
 
-    /**
-     * Return the index of the first occurrence of number in the arr array.
-     * If there is no such element in the array, return -1.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [99, -7, 102], number = -7    ->   2
-     * arr = [5, -3, -4],   number = 10    ->  -1
-     */
     public int findIndexOfNumber(int[] arr, int number) {
-
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == number) {
+                return i;
+            }
+        }
+        return -1;
     }
 
-    /**
-     * Return the new String[] array obtained from the arr array
-     * by reversing the order of the elements.
-     * <p>
-     * Example:
-     * <p>
-     * arr = ["Bob", "Nick"]               -> ["Nick", "Bob"]
-     * arr = ["pineapple", "apple", "pen"] -> ["pen", "apple", "pineapple"]
-     */
     public String[] reverseArray(String[] arr) {
-
+        String[] reversedArray = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reversedArray[i] = arr[arr.length - i - 1];
+        }
+        return reversedArray;
     }
 
-    /**
-     * Return new int[] array obtained from arr int[] array
-     * by choosing positive numbers only.
-     * P.S. 0 is not a positive number =)
-     * <p>
-     * Example:
-     * <p>
-     * arr = [1,-2, 3]      -> [1, 3]
-     * arr = [-1, -2, -3]   -> []
-     * arr = [1, 2]         -> [1, 2]
-     */
     public int[] getOnlyPositiveNumbers(int[] arr) {
-
+        int count = 0;
+        for (int num : arr) {
+            if (num > 0) {
+                count++;
+            }
+        }
+        int[] positiveNumbers = new int[count];
+        int index = 0;
+        for (int num : arr) {
+            if (num > 0) {
+                positiveNumbers[index++] = num;
+            }
+        }
+        return positiveNumbers;
     }
 
-    /**
-     * Return a sorted, ragged, two-dimensional int[][] array following these rules:
-     * Incoming one-dimensional arrays must be arranged in ascending order of their length;
-     * numbers in all one-dimensional arrays must be in ascending order.
-     * <p>
-     * Example:
-     * <p>
-     * arr = [[3, 1, 2,], [3,2]] -> [[2, 3], [1, 2, 3]]
-     * arr = [[5, 4], [7]]       -> [[7], [4, 5]]
-     */
     public int[][] sortRaggedArray(int[][] arr) {
-
+        Arrays.sort(arr, Comparator.comparingInt(a -> a.length));
+        for (int[] subArr : arr) {
+            Arrays.sort(subArr);
+        }
+        return arr;
     }
 }
--- src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
+++ src/main/java/com/epam/mjc/stage0/FridgeAlgorithm.java
@@ -11,6 +11,26 @@ public class FridgeAlgorithm {
      * P.S All methods of interaction with the fridge can be found inside the {@see Fridge} class.
      */
     public void fridgeAlgorithm(Fridge fridge) {
+        // Open the fridge
+        fridge.open();
 
+        // Check if there's milk
+        if (fridge.hasMilk()) {
+            // If there is milk, take it
+            fridge.takeMilk();
+        } else {
+            // If there's no milk, close the fridge and go buy some
+            fridge.close();
+            fridge.goToStore();
+            fridge.buyMilk();
+            // After buying milk, return home and open the fridge again
+            fridge.returnHome();
+            fridge.open();
+            // Take the milk from the fridge
+            fridge.takeMilk();
+        }
+
+        // Close the fridge after taking the milk
+        fridge.close();
     }
 }
